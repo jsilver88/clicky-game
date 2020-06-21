4070ddfa4ac7cb29ab48bff0d176dd450b263c18
Jared Silver <jrsilver88@gmail.com>
2020-06-20 21:06:25 -0700

    build folder deployed

---
+++ build/precache-manifest.6938d66af6cef0b027ed3c9af2c770f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "502c250e5c3cc32d66985b2cd87332cf",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "83e0956aa9f2c396a005",
+    "url": "/clicky-game/static/css/main.3b0aa8f1.chunk.css"
+  },
+  {
+    "revision": "ee68d53e3810843daeb1",
+    "url": "/clicky-game/static/js/2.b1f20aee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.b1f20aee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83e0956aa9f2c396a005",
+    "url": "/clicky-game/static/js/main.40adb723.chunk.js"
+  },
+  {
+    "revision": "a2dd66fdceb11b57b463",
+    "url": "/clicky-game/static/js/runtime-main.3c5f3143.js"
+  }
+]);
