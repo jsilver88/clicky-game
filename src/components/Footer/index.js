77b8e8d4b23fefb43a87e8a3eb22e88a99e3321f
Jared Silver <jrsilver88@gmail.com>
2020-06-20 20:11:43 -0700

    added css styling and changed the color scheme

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,7 @@ const Footer = props => (
     <footer className="footer-container">
         <div className="footer-copyright">
             <div className="container blue darken-4">
-                <p className="white-text flow-text">© 2020 Copyright Jared Silver. For project code <a className="white-text" href="https://github.com/jsilver88/clicky-game">Click here</a></p>
+                <p className="white-text">© 2020 Copyright Jared Silver. For project code <a className="white-text" href="https://github.com/jsilver88/clicky-game">Click here</a></p>
             </div>
         </div>
     </footer>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ import './header.css';
 const Header = props => (
     <div id="header" className="container">
         <div className="col">
-            <span className="headerSection">
+            <span>
                 <h4 id="rules">Click on an image to earn points, but don't click on any of them more than once!</h4>
             </span>
         </div>
